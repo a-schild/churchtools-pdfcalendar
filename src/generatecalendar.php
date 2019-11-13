69992bc699b4b7055061384b3de5c3c56403ea5a
a-schild <andre@schild.ws>
2019-11-13 11:03:50 +0100

    Improved excel generation

--- src/generatecalendar.php
+++ src/generatecalendar.php
@@ -8,6 +8,11 @@ use \PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 
 $printLegende = true;
+$excelTitleFontSize= 20;        // Font size of title line
+$excelHeaderBGColor= 'dddddd';  // Background color of header line
+$excelHeaderFontSize= 12;       // Font size of header line
+$excelEvenBGColor= 'eeeeee';    // Background color of even month lines (Only with full year)
+$excelDateColWidth= 15;
 
 $buildPDF= true;
 $buildXLSX= true;
@@ -15,6 +20,15 @@ if (isset($_REQUEST['outputFormatXLSX']))
 {
     $buildPDF= false;
     $buildXLSX= true;
+}
+
+
+function cellColor($sheet,$cells,$color){
+    $sheet->getStyle($cells)
+    ->getFill()
+    ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+    ->getStartColor()
+    ->setRGB($color); //i.e,colorcode=D3D3D3;
 }
 
 session_start();
@@ -216,20 +230,81 @@ try
                     // Make XLSX
                     $cal = new Spreadsheet();
                     $sheet = $cal->getActiveSheet();
+                    if ($paperFormat == 'A5')
+                    {
+                        $sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A5);
+                    }
+                    else if ($paperFormat == 'A4')
+                    {
+                        $sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4);
+                    }
+                    else if ($paperFormat == 'A3')
+                    {
+                        $sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A3);
+                    }
+                    else if ($paperFormat == 'A2')
+                    {
+                        $sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A2_PAPER);
+                    }
+                    else
+                    {
+                        $sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4);
+                    }
+                    if ($landscape)
+                    {
+                        $sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
+                        $sheet->getPageMargins()->setTop(0.25);
+                        $sheet->getPageMargins()->setRight(0.5);
+                        $sheet->getPageMargins()->setLeft(0.5);
+                        $sheet->getPageMargins()->setBottom(0.25);       
+                    }
+                    else
+                    {
+                        $sheet->getPageMargins()->setTop(0.5);
+                        $sheet->getPageMargins()->setRight(0.25);
+                        $sheet->getPageMargins()->setLeft(0.25);
+                        $sheet->getPageMargins()->setBottom(0.5);       
+                    }
+                    //$sheet->getPageSetup()->setFitToWidth(1);  // Scale to 1 page width
+                    //$sheet->getPageSetup()->setFitToHeight(0); // Don't scale to height
+                    $sheet->getStyle( 'A'.$rowPos )->getFont()->setBold( true )->setSize($excelTitleFontSize);
                     $sheet->setCellValue('A'.$rowPos++, $caption);
                     $myCol= 'A';
                     if ($printLegende)
                     {
+                        $sheet->getStyle( $myCol.$rowPos )->getFont()->setBold( true )->setSize($excelHeaderFontSize);
+                        $sheet->getColumnDimension($myCol)->setAutoSize(true);
+                        cellColor($sheet, $myCol.$rowPos, $excelHeaderBGColor);
                         $sheet->setCellValue($myCol.$rowPos, 'Kalender');
                         $myCol++;
                     }
+                    $sheet->getStyle( $myCol.$rowPos )->getFont()->setBold( true )->setSize($excelHeaderFontSize);
+                    $sheet->getColumnDimension($myCol)->setWidth($excelDateColWidth);
+                    cellColor($sheet, $myCol.$rowPos, $excelHeaderBGColor);
                     $sheet->setCellValue($myCol++.$rowPos, 'Start');
                     if ($printEND)
                     {
+                        $sheet->getStyle( $myCol.$rowPos )->getFont()->setBold( true )->setSize($excelHeaderFontSize);
+                        $sheet->getColumnDimension($myCol)->setWidth($excelDateColWidth);
+                        cellColor($sheet, $myCol.$rowPos, $excelHeaderBGColor);
                         $sheet->setCellValue($myCol++.$rowPos, 'Ende');
                     }
+                    $sheet->getStyle( $myCol.$rowPos )->getFont()->setBold( true )->setSize($excelHeaderFontSize);
+                    $sheet->getColumnDimension($myCol)->setAutoSize(true);
+                    cellColor($sheet, $myCol.$rowPos, $excelHeaderBGColor);
                     $sheet->setCellValue($myCol++.$rowPos, 'Titel');
+                    $sheet->getStyle( $myCol.$rowPos )->getFont()->setBold( true )->setSize($excelHeaderFontSize);
+                    //$sheet->getColumnDimension($myCol)->setAutoSize(true);
+                    cellColor($sheet, $myCol.$rowPos, $excelHeaderBGColor);
                     $sheet->setCellValue($myCol++.$rowPos, 'Bemerkung');
+                    $sheet->getStyle( $myCol.$rowPos )->getFont()->setBold( true )->setSize($excelHeaderFontSize);
+                    //$sheet->getColumnDimension($myCol)->setAutoSize(true);
+                    cellColor($sheet, $myCol.$rowPos, $excelHeaderBGColor);
+                    $sheet->setCellValue($myCol++.$rowPos, 'Weitere infos');
+                    $sheet->getStyle( $myCol.$rowPos )->getFont()->setBold( true )->setSize($excelHeaderFontSize);
+                    $sheet->getColumnDimension($myCol)->setAutoSize(true);
+                    cellColor($sheet, $myCol.$rowPos, $excelHeaderBGColor);
+                    $sheet->setCellValue($myCol++.$rowPos, 'Link');
                     $rowPos++;
                 }
             }
@@ -261,11 +336,13 @@ try
                     $endDate = $entry->getEndDate();
                     $title     = $entry->getTitle();
                     $remarks   = $entry->getRemarks();
-                    if ($remarks != null && strlen(trim($remarks)) > 0) {
-                        $title = $title.' ('.$remarks.')';
-                    }
+                    $moreInfos   = $entry->getMoreInfos();
+                    $link   = $entry->getLink();
                     if ($buildPDF)
                     {
+                        if ($remarks != null && strlen(trim($remarks)) > 0) {
+                            $title = $title.' ('.$remarks.')';
+                        }
                         $cal->addEntry($startDate, $endDate, $title, $calendar->getTextColor(),
                             $calendar->getColor());
                     }
@@ -277,13 +354,31 @@ try
                             $sheet->setCellValue($myCol.$rowPos, $calendar->getName());
                             $myCol++;
                         }
-                        $sheet->setCellValue($myCol++.$rowPos, $startDate);
+                        $sheet->getStyle($myCol.$rowPos)
+                            ->getNumberFormat() 
+                            ->setFormatCode( 
+                            \PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_DATE_DATETIME 
+                            ); 
+                        // Convert to an Excel date/time 
+                        $excelStartDate= \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel( 
+                                          $startDate );  
+                        $sheet->setCellValue($myCol++.$rowPos, $excelStartDate);
                         if ($printEND)
                         {
-                            $sheet->setCellValue($myCol++.$rowPos, $endDate);
+                            $sheet->getStyle($myCol.$rowPos)
+                                ->getNumberFormat() 
+                                ->setFormatCode( 
+                                \PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_DATE_DATETIME 
+                                ); 
+                            // Convert to an Excel date/time 
+                            $excelEndDate = \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel( 
+                                              $endDate );  
+                            $sheet->setCellValue($myCol++.$rowPos, $excelEndDate);
                         }
                         $sheet->setCellValue($myCol++.$rowPos, $title);
                         $sheet->setCellValue($myCol++.$rowPos, $remarks);
+                        $sheet->setCellValue($myCol++.$rowPos, $moreInfos);
+                        $sheet->setCellValue($myCol.$rowPos, $link);
                         if ($printLegende)
                         {
                             $sheet->getStyle("A".$rowPos.":".$myCol.$rowPos)->getFont()->getColor()->
@@ -292,6 +387,16 @@ try
                                     setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->
                                     getStartColor()->
                                     setARGB(aschild\PDFCalendarBuilder\ColorNames::html2html($calendar->getColor(), false));
+                        }
+                        else if ($printFullYear)
+                        {
+                            if ($startDate->format("m") % 2 == 0)
+                            {
+                                $sheet->getStyle("A".$rowPos.":".$myCol.$rowPos)->getFill()->
+                                        setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->
+                                        getStartColor()->
+                                        setRGB($excelEvenBGColor);
+                            }
                         }
                         $rowPos++;
                     }
@@ -321,13 +426,34 @@ try
                         {
                             $sheet->setCellValue($myCol++.$rowPos, $resource->getDescription());
                         }
-                        $sheet->setCellValue($myCol++.$rowPos, $startDate);
+                        // Set the number format mask so that the excel timestamp  
+                        // will be displayed as a human-readable date/time 
+                        $sheet->getStyle($myCol.$rowPos)
+                            ->getNumberFormat() 
+                            ->setFormatCode( 
+                            \PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_DATE_DATETIME 
+                            ); 
+                        // Convert to an Excel date/time 
+                        $excelStartDate = \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel( 
+                                          $startDate );  
+                        $sheet->setCellValue($myCol++.$rowPos, $excelStartDate);
                         if ($printEND)
                         {
-                            $sheet->setCellValue($myCol++.$rowPos, $endDate);
+                            // Set the number format mask so that the excel timestamp  
+                            // will be displayed as a human-readable date/time 
+                            $sheet->getStyle($myCol.$rowPos) 
+                                ->getNumberFormat() 
+                                ->setFormatCode( 
+                                \PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_DATE_DATETIME
+                                ); 
+                            // Convert to an Excel date/time 
+                            $excelEndDate = \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel( 
+                                              $endDate );  
+                            $sheet->setCellValue($myCol++.$rowPos, $excelEndDate);
                         }
                         $sheet->setCellValue($myCol++.$rowPos, $title);
                         $sheet->setCellValue($myCol++.$rowPos, $remark);
+                        $sheet->setCellValue($myCol++.$rowPos, var_export($entry, true));
                         $rowPos++;
                     }
                 }
